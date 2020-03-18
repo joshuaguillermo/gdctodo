0833d5923c196a7b451778fb2a21b55a47b75266
Joshua Guillermo <joshuaguillermo24@gmail.com>
2020-03-18 21:35:53 +0800

    adding node js

---
+++ index.js
@@ -0,0 +1,15 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const username = formData.get('username');
+    const password = formData.get('password');
+
+    const login = {
+        username,
+        password
+    };
+
+    console.log(login);
+})
